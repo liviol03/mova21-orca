48df829282a6f1f262e44aa918f41a12480fbc31
Diego Steiner <diego.steiner@u041.ch>
2021-07-18 21:23:14 +0200

    Small fixes

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,5 +2,5 @@
 
 module Orca
   CAMP_START = Date.parse(ENV.fetch('CAMP_START', '2022-07-23'))
-  CAMP_END = Date.parse(ENV.fetch('CAMP_END', '2022-07-23'))
+  CAMP_END = Date.parse(ENV.fetch('CAMP_END', '2022-08-06'))
 end
